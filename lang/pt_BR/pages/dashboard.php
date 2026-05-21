453eb3fdbf6e104b372ae8334585463d5a599569
Elisame Araújo <elisamearaujo@gmail.com>
2026-05-21 10:04:56 -0300

    fix: description to explicit Brazilian Portuguese language in "locales" key

--- lang/pt_BR/pages/dashboard.php
+++ lang/pt_BR/pages/dashboard.php
@@ -20,7 +20,7 @@ return [
         ],
         'locales' => [
             'label' => 'Idiomas preparados',
-            'description' => 'Interface com arquivos em Português e Inglês.',
+            'description' => 'Interface com arquivos em Português do Brasil e Inglês.',
         ],
         'pages' => [
             'label' => 'Páginas da demo',
